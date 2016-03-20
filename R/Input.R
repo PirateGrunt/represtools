64e8af98b9ff436887c2be5400ee249565c71d0e
Brian Fannin <FanninQED@Yahoo.com>
2016-03-19 22:30:53 -0400

    Initial commit

---
+++ R/Input.R
@@ -0,0 +1,23 @@
+#' @export
+#'
+#' @name LoadInput
+#'
+#' @return Character vector of names of loaded objects
+#'
+#'
+LoadInput <- function(params){
+
+  if (setdiff(c("inputDir", "inputFiles"), names(params))){
+    stop ("Missing inputDir or inputFiles from params.")
+  }
+
+  loadFiles <- paste0(params$inputDir, params$inputFiles, ".rda")
+  lapply(loadFiles, load, .GlobalEnv)
+  loadedObjects <- ls()
+  loadedObjects <- loadedObjects[loadedObjects != "params"]
+  loadedObjects
+}
+
+NamesToObjects <- function(lst){
+
+}
