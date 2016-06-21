6e62219e4956e29e8d18c0deed089943d4fabbcb
Brian Fannin <FanninQED@Yahoo.com>
2016-06-20 21:13:09 -0400

    Use env to search for make

--- R/Make.R
+++ R/Make.R
@@ -1,13 +1,48 @@
+FindMake <- function(){
+
+  strMakeCommand <- Sys.getenv("MAKE")
+
+  if (nchar(strMakeCommand) == 0) strMakeCommand <- "make"
+
+  unname(Sys.which(strMakeCommand))
+
+}
+
 #' Run the make command
 #'
+#' This will run the \code{make} program with whatever target the user selects. The default target is "all". The function works by calling
+#' the \code{\link[base]{system2}} function. It is presumed that an appropriate \code{make} utilty exists on the user's computer. Further
+#' note that the default makefile for an analysis project uses GNU extensions like wildcards.
+#'
+#' The function will to try to confirm
+#'
 #' @param target The make target to run
+#' @param command User-supplied location of the \code{make} command
+#' @param ... Additional arguments to the \code{\link[base]{system2}} function
 #'
 #' @return NULL
 #' @export
-Make <- function(target){
-  strMake <- "make"
+#'
+#' @seealso \code{\link[base]{system2}} \code{\link[base]{system}} \code{\link[base]{Sys.which}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' Make()
+#' Make("clean", command = "/usr/bin/gmake")
+#' Make("gather")
+#' Make("MyFile.rda")
+#' }
+Make <- function(target = "all", command = "", ...){
+
   if (missing(target)) target <- "all"
 
-  strMake <- paste(strMake, target, sep = " ")
-  system(strMake)
+  if (missing(command)){
+    command <- FindMake()
+
+    if (nchar(command) == 0) stop("Could not find make command.")
+
+  }
+
+  system2(command, args = target, ...)
 }
